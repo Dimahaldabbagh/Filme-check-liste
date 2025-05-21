a2955c96aefc7a23fc712c569df2f305b34e37bb
Dimahaldabbagh <dimahaldabbagh@gmail.com>
2025-05-21 17:01:55 +0200

    M4

--- src/main/java/de/htwberlin/webtech/webtech/Film.java
+++ src/main/java/de/htwberlin/webtech/webtech/Film.java
@@ -1,57 +1,39 @@
 package de.htwberlin.webtech.webtech;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Film {
 
-        private Long id;
-        private String titel;
-        private String genre;
-        private int bewertung;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        public Film() {}
+    private String titel;
+    private String genre;
+    private int bewertung;
 
-        public Film(Long id, String titel, String genre, int bewertung) {
-            this.id = id;
-            this.titel = titel;
-            this.genre = genre;
-            this.bewertung = bewertung;
-        }
-        @Id
-        public Long getId() {
-            return id;
-        }
+    public Film() {}
 
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getTitel() {
-            return titel;
-        }
-
-        public void setTitel(String titel) {
-            this.titel = titel;
-        }
-
-        public String getGenre() {
-            return genre;
-        }
-
-        public void setGenre(String genre) {
-            this.genre = genre;
-        }
-
-        public int getBewertung() {
-            return bewertung;
-        }
-
-        public void setBewertung(int bewertung) {
-            this.bewertung = bewertung;
-        }
+    public Film(Long id, String titel, String genre, int bewertung) {
+        this.id = id;
+        this.titel = titel;
+        this.genre = genre;
+        this.bewertung = bewertung;
     }
 
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
+    public String getTitel() { return titel; }
+    public void setTitel(String titel) { this.titel = titel; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public int getBewertung() { return bewertung; }
+    public void setBewertung(int bewertung) { this.bewertung = bewertung; }
+}
